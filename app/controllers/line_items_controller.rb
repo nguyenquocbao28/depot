a8c9ac4437bcc8c1352b81b16e188a07d8d547dd
nguyenquocbao28 <nguyenquocbao758@gmail.com>
2020-03-23 02:28:47 -0700

    Iteration F3: Highlighting Changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,9 +32,10 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
+        format.js{ @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
-        format.html { render :new }
+        format.html { render :show }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
